4bc56a1275e35032c87446c3b53f6e20178df514
xubh <brian.xu@luttica.com>
2019-07-09 15:29:19 +0800

    删除不必要资源，采用CDN

--- src/main/java/com/ryoua/myblog/config/MyWebMvcConfigurerAdapter.java
+++ src/main/java/com/ryoua/myblog/config/MyWebMvcConfigurerAdapter.java
@@ -50,8 +50,6 @@ public class MyWebMvcConfigurerAdapter extends WebMvcConfigurerAdapter {
     /**
      * 以前要访问一个页面需要先创建个Controller控制类，在写方法跳转到页面
      * 在这里配置后就不需要那么麻烦了，直接访问http://10.2.3.235:80/toLogin就跳转到login.html页面了
-     *
-     * @param registry
      */
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
@@ -61,8 +59,6 @@ public class MyWebMvcConfigurerAdapter extends WebMvcConfigurerAdapter {
 
     /**
      * 拦截器
-     *
-     * @param registry
      */
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
